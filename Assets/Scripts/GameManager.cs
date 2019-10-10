03ffbc8a8db6e66739a19eacbc2101a7174e6477
SpeakingSword <45556848+SpeakingSword@users.noreply.github.com>
2019-10-10 14:40:13 +0800

    第一次提交

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float gravityModifier = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.gravity *= gravityModifier;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveForce = 10.0f;       // 推动玩家前进的力，决定了玩家的移动速度
+    [SerializeField] private float jumpForce = 100.0f;      // 玩家跳跃的力，决定玩家跳跃的高度
+    private Rigidbody2D playerRigidbody2D;
+
+    public TextMeshProUGUI moveSpeedText;                   // 在屏幕上显示玩家的移动速度
+
+    private void Awake()
+    {
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerJump();
+        }
+    }
+
+    private void Update()
+    {
+        moveSpeedText.text = "MoveSpeed: " + Mathf.RoundToInt(playerRigidbody2D.velocity.x);
+    }
+
+    void PlayerMove()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if(horizontalInput > 0)
+        {
+            transform.localScale = new Vector2(0.1f, 0.1f);
+            playerRigidbody2D.AddForce(new Vector2(moveForce, 0));
+        }
+        else if(horizontalInput < 0)
+        {
+            transform.localScale = new Vector2(-0.1f, 0.1f);
+            playerRigidbody2D.AddForce(new Vector2(-moveForce, 0));
+        }
+    }
+
+    void PlayerJump()
+    {
+            playerRigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+}
