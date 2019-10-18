53c623ffde2796e3269cd11fc2e9b8564fa88c0c
CoolLe <1248639799@qq.com>
2019-10-18 09:18:51 +0800

    背景循环？

---
+++ Assets/StartPage/BackgroundScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    public float speed = -2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f,speed * Time.deltaTime,0f);
+        if(transform.position.y<=-15)
+        {
+            transform.Translate(0f,30f,0f);
+        }
+    }
+}
